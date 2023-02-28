60cddc178f67c3b666312489ee1366f2648da3ba
LuisXIV01 <111925109+LuisXIV01@users.noreply.github.com>
2023-02-28 14:46:09 -0300

    Update 871 _Counting_Cells_in_a_Blob.cpp

--- 871 _Counting_Cells_in_a_Blob.cpp
+++ 871 _Counting_Cells_in_a_Blob.cpp
@@ -5,14 +5,14 @@ bool vis[N][N];
 char adjlist[N][N];
 int dx[] = {-1,0,1,0,-1,-1,1,1};
 int dy[] = {0,1,0,-1,1,-1,-1,1};
-int x, y, t;
+int x, y, tlin, tcol;
 void dfs(int i, int j, int* cont){
     vis[i][j] = true;
     for(int k=0; k<8; k++){
 		x = dx[k] + i;
 		y = dy[k] + j;
 		
-        if(x >-1 and x < t and y >-1 and y < t and adjlist[x][y] == '1' and !vis[x][y]){
+        if(x >-1 and x < tlin and y >-1 and y < tcol and adjlist[x][y] == '1' and !vis[x][y]){
 			//cout << "x = " << x << " y = " << y << '\n';
 			(*cont)++;
             dfs(x,y, cont);
@@ -21,37 +21,41 @@ void dfs(int i, int j, int* cont){
 }
 int main(){
     freopen("output.txt", "w", stdout);
-    int i, j, k, c, pos, cont, maior;
-    string text;
+    freopen("imput.txt", "r", stdin);
+    int i, j, k, c, cont, maior;
+    string text[25];
     cin >> c;
+    cin.ignore();
     for(k = 0; k<c; k++){
-		cin.ignore();
 		if(k) cout << '\n';
-		cin >> text;
-		t = text.size();
-		for(i=0; i<t; i++){
-			adjlist[0][i] = text[i];
+		
+		getline(cin, text[0]);
+		for(i=1; text[i-1].size() !=0; i++){
+			getline(cin, text[i]);
 		}
-		for(i=1, pos=0; i<t; ++i){
-			for(j=0; j<t; ++j, pos++){
-				cin >> adjlist[i][j];
+		tlin=i-1;
+		tcol = text[0].size();
+		for(i=0; i<tlin; ++i){
+			for(j=0; j<tcol; ++j){
+				adjlist[i][j] = text[i][j];
 			}
 		}
-		for(i=0, maior=cont=1; i<t; ++i){
-			for(j=0; j<t; ++j){
+		for(i=0, maior=cont=0; i<tlin; ++i){
+			for(j=0; j<tcol; ++j){
 				if(adjlist[i][j] == '1'){
+					cont++;
 					if(!vis[i][j]){
 						dfs(i, j, &cont);
 						if(cont > maior) maior = cont;
-						cont =1;
+						cont =0;
 					}
 				}
 				//cout << "aqui\n";
 			}
 		}
 		cout << maior << '\n';
-		for(i=0; i<t; ++i){
-			for(j=0; j<t; ++j){
+		for(i=0; i<tlin; ++i){
+			for(j=0; j<tcol; ++j){
 				vis[i][j] = false;
 			}
 		}
