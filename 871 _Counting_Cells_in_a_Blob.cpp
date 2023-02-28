0905c102986e17ca23e3fb5a4a5d6ad82ec19abe
LuisXIV01 <111925109+LuisXIV01@users.noreply.github.com>
2023-02-28 15:56:42 -0300

    Update 871 _Counting_Cells_in_a_Blob.cpp

--- 871 _Counting_Cells_in_a_Blob.cpp
+++ 871 _Counting_Cells_in_a_Blob.cpp
@@ -20,11 +20,12 @@ void dfs(int i, int j, int* cont){
     }
 }
 int main(){
-    freopen("output.txt", "w", stdout);
-    freopen("imput.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    //freopen("imput.txt", "r", stdin);
     int i, j, k, c, cont, maior;
-    string text[25];
+    string text[25], te;
     cin >> c;
+    getline(cin, te);
     cin.ignore();
     for(k = 0; k<c; k++){
 		if(k) cout << '\n';
@@ -43,8 +44,8 @@ int main(){
 		for(i=0, maior=cont=0; i<tlin; ++i){
 			for(j=0; j<tcol; ++j){
 				if(adjlist[i][j] == '1'){
-					cont++;
 					if(!vis[i][j]){
+						cont++;
 						dfs(i, j, &cont);
 						if(cont > maior) maior = cont;
 						cont =0;
