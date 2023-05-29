8877de6f8348d8d78e94466a0f705de44c519081
LuisXIV01 <111925109+LuisXIV01@users.noreply.github.com>
2023-05-29 14:20:56 -0300

    Update 852_Deciding_victory_in_Go.cpp

--- 852_Deciding_victory_in_Go.cpp
+++ 852_Deciding_victory_in_Go.cpp
@@ -1,44 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define N 9
-bool vis[N][N], ver, ok;
+bool vis[N][N], vez1, ok;
 char adjlist[N][N], let;
 int dx[] = {-1,0,1,0,-1,-1,1,1};
 int dy[] = {0,1,0,-1,1,-1,-1,1};
-int x, y, black, white, meio;
-void dfs(int i, int j, char l){
+int x, y;
+map<char, int> color;
+
+void dfs(int i, int j){
     vis[i][j] = true;
     for(int k=0; k<4; k++){
 		x = dx[k] + i;
 		y = dy[k] + j;
-		if(x >-1 and x < N and y >-1 and y < N and adjlist[x][y] == 'X' and !vis[x][y] and !ok){
-			let = 'X';
-			ok = true;
+		if(x >-1 and x < N and y >-1 and y < N and !vis[x][y]){
+			if(adjlist[x][y] != '.'){
+				color[adjlist[x][y]]++;
+				dfs(x,y);
+			}
 		}
-		else{
-			if(x >-1 and x < N and y >-1 and y < N and adjlist[x][y] == 'O' and !vis[x][y] and !ok){
-				let = 'O';
-				ok = true;
+    }
+}
+void dfsMeio(int i, int j){
+    vis[i][j] = true;
+    for(int k=0; k<4; k++){
+		x = dx[k] + i;
+		y = dy[k] + j;
+		if(x >-1 and x < N and y >-1 and y < N and !vis[x][y]){
+			if(adjlist[x][y] == '.'){
+				color[adjlist[x][y]]++;
+				dfsMeio(x,y);
 			}
 			else{
-				if(x >-1 and x < N and y >-1 and y < N and adjlist[x][y] != l and !vis[x][y] and adjlist[x][y] != let and ok) ver = false;
+				if(!vez1){ // primeira vez aqui
+					let = adjlist[x][y];
+					vez1 = true;
+				}
+				else{
+					if(let != adjlist[x][y]){
+						ok = true;
+					}
+				}
 			}
 		}
-		
-        if(x >-1 and x < N and y >-1 and y < N and adjlist[x][y] == l and !vis[x][y]){
-			if(l == 'X') black++;
-			else{
-				if(l == 'O') white++;
-				else meio++;
-			}
-            dfs(x,y, l);
-		}
-		
     }
 }
 int main(){
-    freopen("output.txt", "w", stdout);
-    freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    //freopen("input.txt", "r", stdin);
     int i, j, k, t;
     cin >> t;
     for(k = 0; k<t; k++){
@@ -47,37 +56,40 @@ int main(){
 				cin >> adjlist[i][j];
 			}
 		}
-		black = white = 0;
+		//~ for(i=0; i<N; ++i){
+			//~ for(j=0; j<N; ++j){
+				//~ cout << adjlist[i][j];
+			//~ }
+			//~ cout << '\n';
+		//~ }
 		for(i=0; i<N; ++i){
 			for(j=0; j<N; ++j){
-				meio = 0;
-				ver = true;
-				ok = false;
-				if(adjlist[i][j] == 'X'){
-					if(!vis[i][j]){
-						dfs(i,j,'X');
-					}
+				if(adjlist[i][j] != '.' and !vis[i][j]){
+					color[adjlist[i][j]]++;
+					dfs(i,j);
 				}
-				else{
-					if(adjlist[i][j] == 'O'){
-						if(!vis[i][j]){
-							dfs(i,j,'O');
-						}
-					}
-					else{
-						if(!vis[i][j]){
-							dfs(i,j,'.');
-						}
-						if(ver and let == 'X') black += meio;
-						else{
-							if(ver and let == 'O') white += meio;
-						}
-					}
-				}
-				//cout << "aqui\n";
 			}
 		}
-		cout << "Black " << black << " White " << white << '\n';
+		for(i=0; i<N; ++i){
+			for(j=0; j<N; ++j){
+				vis[i][j] = false;
+			}
+		}
+		for(i=0; i<N; ++i){
+			for(j=0; j<N; ++j){
+				if(adjlist[i][j] == '.' and !vis[i][j]){
+					let = '.';
+					vez1 = ok = false;
+					color['.'] =1;
+					dfsMeio(i,j);
+					if(!ok){
+						color[let] += color['.'];
+					}
+				}
+			}
+		}
+		cout << "Black " << color['X'] << " White " << color['O'] << '\n';
+		color.clear();
 		for(i=0; i<N; ++i){
 			for(j=0; j<N; ++j){
 				vis[i][j] = false;
