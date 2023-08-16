acf9da03d0bba37985ad71fb9a1aebf1322180c0
LuisXIV01 <111925109+LuisXIV01@users.noreply.github.com>
2023-08-16 14:58:27 -0300

    Update 429_Word_Transformation.cpp

--- BFS/429_Word_Transformation.cpp
+++ BFS/429_Word_Transformation.cpp
@@ -1,32 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector <string>  dictionary;
-map <string, bool> vis;
-void makeTree(string a){
-	queue <string> q;
-	
-	while(!q.empty()){
-		
-	}
-}
+map <string, pair<int, bool>> dic;
 bool umaLetra(string a, string b){
-	map <char, int> x;
-	for(int i = 0; i < a.size(); i++){
-		x[a[i]]++;
-		x[b[i]]++;
+	
+	int carac=0;
+	
+	for(int i = 0; i <(int)a.size(); i++){
+		if(a[i]!=b[i]) carac++;
 	}
-	return (x.size() == a.size()+1);
+	return (carac==1);
+	
+}
+void makeTree(string a, string b){
+	queue <string> q;
+	q.push(a);
+	dic[a].second = true;
+	//cout << q.front() << ' ' << b << '\n';
+	while(!q.empty() and q.front() != b){
+		for (auto k = dic.begin(); k != dic.end(); ++k){
+			if(!(k->second.second) and (k->first).size() == (q.front()).size() and umaLetra(k->first, q.front())){
+				k->second.second = true;
+				k->second.first = dic[q.front()].first + 1;
+				q.push(k->first);
+			}
+		}
+		q.pop();
+	}
 }
 int main(){
 	
-	string a;
+	string a, b, c;
 	int n;
+	//~ freopen("12","w",stdout);
+	//~ freopen("input.txt", "r", stdin);
 	cin >> n;
+	getline(cin.ignore(),a);
+	
 	for(int i=0; i < n; i++){
 		cin >> a;
-		dictionary.push_back(a);
-		vis[a] = false;
+		while(a != "*"){
+			dic[a] = {0,false};
+			cin >> a;
+		}
+		cin.ignore();
+		while(getline(cin,b) and (int)b.size() > 0){
+			stringstream ss;
+			ss << b;
+			ss >> b >> c;
+			makeTree(b, c);
+			cout << b << ' ' << c << ' ' << dic[c].first << '\n';
+			for (auto k = dic.begin(); k != dic.end(); ++k){
+				k->second.first = 0;
+				k->second.second = false;
+			}
+		}
+		dic.clear();
+		if(n>0) cout<<endl;
 	}
-	
 }
-if(!vis[dictionary[i]] and dictionary[i].size() == a.size() and umaLetra(dictionary[i], )
