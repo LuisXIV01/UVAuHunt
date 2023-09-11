2f519095d75d20f7312db196148522f2150f5bbe
LuisXIV01 <111925109+LuisXIV01@users.noreply.github.com>
2023-09-11 16:12:04 -0300

    Update and rename incompleto.cpp to 1112_Mice_and_Maze.cpp

--- Dijkstra's/1112_Mice_and_Maze.cpp
+++ Dijkstra's/1112_Mice_and_Maze.cpp
@@ -6,12 +6,14 @@ int size;
 map<int, vector<pair<int,int>>> adjlist;
 int dj(int m, int n){
 	pq q;
-	q.push(adjlist[m][0]);
-	vector<int> v(size +1, INF);
+	
+	vector<int> v(size +5, INF);
 	v[m] = 0;
+	q.push({v[m], m});
 	int xx = m;
 	while(!q.empty()){
 		int carga = q.top().first;
+		xx = q.top().second;
 		q.pop();
 		if(v[xx] < carga) continue;
 		
@@ -19,26 +21,35 @@ int dj(int m, int n){
 			if(v[p.second] > p.first + carga){
 				v[p.second] = carga + p.first;
 				q.push({v[p.second],p.second});
-				xx = p.second;
+				//xx = p.second;
 			}
 		}
 	}
 	return v[n];
 }
 int main(){
-	int N, z, t, q, a, b, c, cont;
+	//freopen("output.txt","w", stdout);
+	int N, z, t, q, a, b, c;
+	bool ok=false;
 	cin >> N;
 	while(N--){
-		cont = 0;
+		if(ok) cout << '\n';
 		cin >> size >> z >> t >> q;
 		for(int i = 0; i < q; i++){
 			cin >> a >> b >> c;
 			adjlist[a].push_back({c,b});
 		}
-		for(int i = 1; i <= size; i++){
-			cout << dj(i,z) << '\n';
-			//if( <= t) cont++;
+		
+		int cont=0;
+		for(int i=1; i <= size; i++){
+			//cout << dj(i,z) << '\n';
+			if(dj(i,z) <= t){
+				cont++;
+			}
 		}
 		cout << cont << '\n';
+		adjlist.clear();
+		ok=true;
 	}
+	
 }
